d6793090e2974891134f9d6d2ecd141f53ccdf64
nitipat009 <nitipatboongate@gmail.com>
2022-05-02 20:26:29 +0700

    Model 1 mix!

--- routes/r_model1.py
+++ routes/r_model1.py
@@ -2,15 +2,16 @@ from fastapi import APIRouter, Response
 from models.model1 import Model1
 import io, base64
 from PIL import Image
-
+from api import remove_watermark
 model1 = APIRouter(prefix="/Model1")
 
 
 @model1.post('/')
 async def get_Image(data:Model1) :
-    with open("imageToSave.png", "wb") as fh:
-        fh.write(base64.decodebytes(data.image_path))
-    return data
+    image_buf = io.BytesIO(base64.decodebytes(data.image_path))
+    mask_buf = io.BytesIO(base64.decodebytes(data.mask_path))
+    img_str = remove_watermark(image_buf,mask_buf,data.MAX_DIM,data.REG_NOISE,data.INPUT_DEPTH,data.LR,data.SHOW_STEP,data.TRAINING_STEPS)
+    return {"image" : img_str}
 
 # def get_image()
 #     image_bytes: bytes = generate_cat_picture()
